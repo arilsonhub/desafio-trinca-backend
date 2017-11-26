ca0a0d1912a1b8b36b444044ab8ccf10fae575d5
arilsonhub <arilson.senac.ads@gmail.com>
2017-11-26 19:59:00 -0200

    Update backup_banco.sql

--- backup_banco.sql
+++ backup_banco.sql
@@ -25,4 +25,61 @@ GO
 
 /* ----------- Tabela Participante --------------- */
 
+USE [churrasTrinca]
+GO
+
+/****** Object:  Table [dbo].[Participantes]    Script Date: 26/11/2017 19:56:47 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Participantes](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[nome] [nvarchar](255) NOT NULL,
+	[valorContribuicao] [money] NOT NULL,
+	[isPago] [int] NOT NULL,
+	[isBebida] [int] NOT NULL,
+	[observacao] [nvarchar](255) NULL,
+	[churrascoId] [bigint] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
 /* ----------- Tabela Usuario --------------- */
+
+USE [churrasTrinca]
+GO
+
+/****** Object:  Table [dbo].[Usuarios]    Script Date: 26/11/2017 19:56:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Usuarios](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[login] [nvarchar](255) NOT NULL,
+	[senha] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/* ----------- INSERTS --------------- */
+USE [churrasTrinca]
+GO
+
+INSERT INTO [dbo].[Usuarios]
+           ([login]
+           ,[senha])
+     VALUES
+           ('arilson','123456')
+GO
